47d81bc64a0f4426e72dbb37426fdfb67a112ba2
uk-2149 <utkal2149@gmail.com>
2025-12-17 16:00:15 +0530

    added analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import OnekoCat from "../components/OnekoCat";
 import TitleWatcher from "../components/TitleWatcher";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} overflow-x-hidden`}>
         <TitleWatcher />
         {children}
+        <Analytics />
         <OnekoCat />
       </body>
     </html>
